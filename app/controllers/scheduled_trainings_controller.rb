26e2c760a7dd1990ac371ee8a474d141553abb69
stiig <vasiliyrps2@gmail.com>
2021-02-09 02:52:24 +0500

    add stub controller

---
+++ app/controllers/scheduled_trainings_controller.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+class ScheduledTrainingsController < ApplicationController
+  def index
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
